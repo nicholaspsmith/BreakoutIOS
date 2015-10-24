572e83c88e6e604cfb9dd5036ec905f126ab99ec
Nicholas Smith <npsmith1990@gmail.com>
2015-10-24 15:44:26 -0500

    Create ball; create bounds; set ball physics

--- demo/GameScene.swift
+++ demo/GameScene.swift
@@ -11,32 +11,27 @@ import SpriteKit
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 45;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        let border = SKPhysicsBody(edgeLoopFromRect: self.frame)
+        border.friction = 0
         
-        self.addChild(myLabel)
+        self.physicsBody = border
+        
+        // Set gravity to 0
+        self.physicsWorld.gravity = CGVectorMake(0,0)
+        
+        let ball = childNodeWithName("Ball") as! SKSpriteNode
+        
+        ball.physicsBody!.applyImpulse(CGVectorMake(30, -30))
+        ball.physicsBody!.allowsRotation = false
+        ball.physicsBody!.friction = 0
+        ball.physicsBody!.restitution = 1
+        ball.physicsBody!.linearDamping = 0
+        ball.physicsBody!.angularDamping = 0
+        
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
-        
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
     }
    
     override func update(currentTime: CFTimeInterval) {
