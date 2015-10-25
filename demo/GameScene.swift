9b0fb853be045ac4a39d8a360e4c9512fd8eb830
Nicholas Smith <npsmith1990@gmail.com>
2015-10-24 19:13:04 -0500

    Make paddle movable

--- demo/GameScene.swift
+++ demo/GameScene.swift
@@ -9,6 +9,9 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var isTouchingPaddle = false
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         let border = SKPhysicsBody(edgeLoopFromRect: self.frame)
@@ -32,15 +35,38 @@ class GameScene: SKScene {
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
-        var touch = touches.first as UITouch!
-        var location = touch.locationInNode(self)
+        let touch = touches.first as UITouch!;
+        let location = touch.locationInNode(self);
+        _ = touch.previousLocationInNode(self);
         
         if let body = self.physicsWorld.bodyAtPoint(location) {
             if body.node!.name == "Paddle" {
-                print("touch")
+                isTouchingPaddle = true
             }
         }
-        
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if isTouchingPaddle {
+            let touch = touches.first as UITouch!
+            let location = touch.locationInNode(self)
+            let prevLocation = touch.previousLocationInNode(self)
+            
+            let paddle = childNodeWithName("Paddle") as! SKSpriteNode
+            
+            // Set position of paddle
+            var xPos = paddle.position.x + (location.x - prevLocation.x)
+            // Set left bound
+            xPos = max(xPos, paddle.size.width / 2)
+            // Set right bound
+            xPos = min(xPos, size.width - paddle.size.width / 2)
+            
+            paddle.position = CGPointMake(xPos, paddle.position.y)
+        }
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        isTouchingPaddle = false
     }
    
     override func update(currentTime: CFTimeInterval) {
